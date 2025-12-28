26599fe826e41063a5c6c99423fbc5c373ed02d1
Shiladitya24042003 <shiladityasaha24042003@gmail.com>
2025-12-28 17:11:43 +0530

    Detect Cycle in a DAG using Toposort Logic

---
+++ Graph/G-23_Detect_Cycle_In_A_DAG_Kahn's_Algorithm.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        vector<int> adj[V];
+        vector<int> indg(V, 0);
+        for(auto it: edges) {
+            int u = it[0];
+            int v = it[1];
+            adj[u].push_back(v);
+            indg[v]++;
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(indg[i] == 0)
+                q.push(i);
+        }
+        int cnt = 0;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            cnt++;
+            
+            for(auto it: adj[node]) {
+                indg[it]--;
+                if(indg[it] == 0) q.push(it);
+            }
+        }
+        
+        if(cnt == V) return false;
+        return true;
+    }
+};

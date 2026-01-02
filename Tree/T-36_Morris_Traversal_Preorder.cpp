8d7d2fa9ecc1d69cc0a63ace3c38d0bceaf5f846
Shiladitya24042003 <shiladityasaha24042003@gmail.com>
2026-01-02 23:20:38 +0530

    Morris Traversal Preorder

---
+++ Tree/T-36_Morris_Traversal_Preorder.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ //Morris Traversal
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        TreeNode* curr = root;
+        while(curr != NULL) {
+            if(curr->left == NULL) {
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else {
+                TreeNode* temp = curr->left;
+                while(temp->right != NULL && temp->right != curr) {
+                    temp = temp->right;
+                }
+                if(temp->right == NULL) {
+                    temp->right = curr;
+                    preorder.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else if(temp->right == curr) {
+                    temp->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+        return preorder;
+    }
+};

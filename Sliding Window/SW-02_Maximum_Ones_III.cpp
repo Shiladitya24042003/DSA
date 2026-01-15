32118b711dbfe83397835f63de5c64d6b6cd574e
Shiladitya24042003 <shiladityasaha24042003@gmail.com>
2026-01-15 20:18:36 +0530

    Maximum Ones III

---
+++ Sliding Window/SW-02_Maximum_Ones_III.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int zero = 0;
+        int l = 0, r = 0;
+        int ans = 0;
+        while(r < n) {
+            if(nums[r] == 0) zero++;
+            if(zero > k) {
+                if(nums[l] == 0) zero--;
+                l++;
+            }
+            ans = max(ans, r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
+
+/**
+    Just change the question statement to this:
+    longest subarray of nums with atmost k zeros
+    
+    And actually this justify too; because when we select a subarray we only can change k zeroes.
+*/
